02d70bfac449aa7cc67576e8b7bce749c792b8bf
Martin Bobbio <martinbobbio1@gmail.com>
2019-10-04 03:37:38 -0300

    Frontend Weather App

---
+++ docs/precache-manifest.6269dbdc2b8d9848aaf88bf1e006f581.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "67c0ca41a6b0977bb7f5",
+    "url": "/static/css/main.7132b107.chunk.css"
+  },
+  {
+    "revision": "67c0ca41a6b0977bb7f5",
+    "url": "/static/js/main.67c0ca41.chunk.js"
+  },
+  {
+    "revision": "b03ab643f959f1b06575",
+    "url": "/static/css/1.829c5078.chunk.css"
+  },
+  {
+    "revision": "b03ab643f959f1b06575",
+    "url": "/static/js/1.b03ab643.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fbf6cb42e94b3c1628062952d2fa600e",
+    "url": "/index.html"
+  }
+];
