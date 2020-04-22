55e36a9fc3b885063de54129daaccaccbd7d9348
Martin Bobbio <martinbobbio1@gmail.com>
2020-04-22 20:43:02 -0300

    Update apiUrl.js

--- src/constants/apiUrl.js
+++ src/constants/apiUrl.js
@@ -1,3 +1,19 @@
-export const BACKEND_WEATHER = "https://martinbobbio-weather.herokuapp.com/v1/current/";
-export const BACKEND_FORECAST = "https://martinbobbio-weather.herokuapp.com/v1/forecast/";
-export const BACKEND_LOCATION = "https://martinbobbio-weather.herokuapp.com/v1/location/";
+let BACKEND_WEATHER, BACKEND_FORECAST, BACKEND_LOCATION = '';
+
+if(process.env.REACT_APP_STAGE === 'prod'){
+  BACKEND_WEATHER = "https://martinbobbio-weather.herokuapp.com/v1/current/";
+  BACKEND_FORECAST = "https://martinbobbio-weather.herokuapp.com/v1/forecast/";
+  BACKEND_LOCATION = "https://martinbobbio-weather.herokuapp.com/v1/location/";
+} else if(process.env.REACT_APP_STAGE === 'docker'){
+  BACKEND_WEATHER = "http://192.168.99.100:8084/v1/current/";
+  BACKEND_FORECAST = "http://192.168.99.100:8084/v1/forecast/";
+  BACKEND_LOCATION = "http://192.168.99.100:8084/v1/location/";
+} else {
+  BACKEND_WEATHER = "http://localhost:8080/v1/current/";
+  BACKEND_FORECAST = "http://localhost:8080/v1/forecast/";
+  BACKEND_LOCATION = "http://localhost:8080/v1/location/";
+}
+
+export const BACKEND_WEATHER;
+export const BACKEND_FORECAST;
+export const BACKEND_LOCATION;
