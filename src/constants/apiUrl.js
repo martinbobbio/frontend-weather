b2fe156360f45289cefb111ce0ce0a664bb96c50
Martin Bobbio <martinbobbio1@gmail.com>
2020-04-22 21:51:53 -0300

    Update apiUrl.js

--- src/constants/apiUrl.js
+++ src/constants/apiUrl.js
@@ -1,3 +1,3 @@
 export const BACKEND_WEATHER = process.env.REACT_APP_STAGE === 'prod' ? 'https://martinbobbio-weather.herokuapp.com/v1/current/' : 'http://192.168.99.100:8084/v1/current/';
-export BACKEND_FORECAST = process.env.REACT_APP_STAGE === 'prod' ? 'https://martinbobbio-weather.herokuapp.com/v1/forecast/' : 'http://192.168.99.100:8084/v1/forecast/';
-export BACKEND_LOCATION = process.env.REACT_APP_STAGE === 'prod' ? 'https://martinbobbio-weather.herokuapp.com/v1/location/' : 'http://192.168.99.100:8084/v1/location/';
+export const BACKEND_FORECAST = process.env.REACT_APP_STAGE === 'prod' ? 'https://martinbobbio-weather.herokuapp.com/v1/forecast/' : 'http://192.168.99.100:8084/v1/forecast/';
+export const BACKEND_LOCATION = process.env.REACT_APP_STAGE === 'prod' ? 'https://martinbobbio-weather.herokuapp.com/v1/location/' : 'http://192.168.99.100:8084/v1/location/';
