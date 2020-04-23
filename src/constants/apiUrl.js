58b95e5b090cd94f8604f3c5e3438825de86db86
Martin Bobbio <martinbobbio1@gmail.com>
2020-04-22 21:51:36 -0300

    Update apiUrl.js

--- src/constants/apiUrl.js
+++ src/constants/apiUrl.js
@@ -1,19 +1,3 @@
-let BACKEND_WEATHER, BACKEND_FORECAST, BACKEND_LOCATION = '';
-
-if(process.env.REACT_APP_STAGE === 'prod'){
-  BACKEND_WEATHER = "https://martinbobbio-weather.herokuapp.com/v1/current/";
-  BACKEND_FORECAST = "https://martinbobbio-weather.herokuapp.com/v1/forecast/";
-  BACKEND_LOCATION = "https://martinbobbio-weather.herokuapp.com/v1/location/";
-} else if(process.env.REACT_APP_STAGE === 'docker'){
-  BACKEND_WEATHER = "http://192.168.99.100:8084/v1/current/";
-  BACKEND_FORECAST = "http://192.168.99.100:8084/v1/forecast/";
-  BACKEND_LOCATION = "http://192.168.99.100:8084/v1/location/";
-} else {
-  BACKEND_WEATHER = "http://localhost:8080/v1/current/";
-  BACKEND_FORECAST = "http://localhost:8080/v1/forecast/";
-  BACKEND_LOCATION = "http://localhost:8080/v1/location/";
-}
-
-export BACKEND_WEATHER;
-export BACKEND_FORECAST;
-export BACKEND_LOCATION;
+export const BACKEND_WEATHER = process.env.REACT_APP_STAGE === 'prod' ? 'https://martinbobbio-weather.herokuapp.com/v1/current/' : 'http://192.168.99.100:8084/v1/current/';
+export BACKEND_FORECAST = process.env.REACT_APP_STAGE === 'prod' ? 'https://martinbobbio-weather.herokuapp.com/v1/forecast/' : 'http://192.168.99.100:8084/v1/forecast/';
+export BACKEND_LOCATION = process.env.REACT_APP_STAGE === 'prod' ? 'https://martinbobbio-weather.herokuapp.com/v1/location/' : 'http://192.168.99.100:8084/v1/location/';
